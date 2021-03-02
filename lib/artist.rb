b553a7c0b4840512eab79a3511f0f4695f7b2f1d
jgumerove <josh.gumerove@gmail.com>
2021-03-02 14:49:12 -0500

    commit tree 3fd7d929da3ad70f42cb5472dbd8a47e6b62a92e

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,28 @@
+class Artist
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select{|song| song.artist == self} #needs to be created before new_song is created
+    end
+
+    def genres
+        songs.collect {|song| song.genre} # look above 
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+
+end
